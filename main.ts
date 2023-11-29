74afb64c1aaf04265780e2b7d60acc497a593f08
william <124389603+william-simard@users.noreply.github.com>
2023-11-29 13:07:36 -0500

    5-08 progression

--- main.ts
+++ main.ts
@@ -1,18 +1,37 @@
 /* Copyright (c) 2023 MTHS All rights reserved
  *
  * Created by: william simard
- * Created on: nov 2023
- * This program ...
+ * Created on: Nov 2023
+ * This program turns a 2 - 28BYJ-48 DC 5V until a sonar sensor senses an object 10cm away
 */
 
-//setup
+// variable
+let distanceToObject: number = 0
+
+// setup
 basic.showIcon(IconNames.Happy)
 
-//loop forever 
 while (true) {
-  if (input.buttonIsPressed(Button.A) == true) {
-      
-    
-  }
+    if (input.buttonIsPressed(Button.A) === true) {
+        // find distance from sonar
+        basic.clearScreen()
+        distanceToObject = sonar.ping(
+            DigitalPin.P1,
+            DigitalPin.P2,
+            PingUnit.Centimeters
+        )
+        basic.showNumber(distanceToObject)
+        basic.pause(500)
+        basic.showIcon(IconNames.Happy)
 
+        if (distanceToObject < 10) {
+            robotbit.StpCarMove(-10, 48)
+            basic.pause(500)
+            robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
+            basic.pause(500)
+            robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B4)
+            basic.pause(500)
+            robotbit.StpCarMove(10, 48)
+        }
+    }
 }
